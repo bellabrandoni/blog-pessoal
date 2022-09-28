cfcb89cffcc3de9a236f9de3dc25a4c5e4975001
unknown <isabella14brandao@gmail.com>
2022-09-28 11:06:38 -0300

    criado pages home e home.css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home () {
+    return (
+        <>
+            <h1 className="titulo" >Home</h1>
+            <img src= "https://danielvoelk.de/wp-content/uploads/humaaans-undraw-alternative.png" alt="imagem tela inicial" className="img"/>
+        </>
+    );
+}
+export default Home;
