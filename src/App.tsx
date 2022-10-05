aa383496d0973039cf0cd44fbfe72fdf69ced5fd
unknown <isabella14brandao@gmail.com>
2022-10-05 16:24:14 -0300

    Adicionado rota para postagem e tema

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Navbar from './components/static/navbar/Navbar'
 import Footer from './components/static/footer/Footer'
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import ListaTema from './components/temas/listatema/ListaTema';
 
 
 
@@ -16,13 +18,18 @@ function App() {
       <Navbar />
       <Routes>
 
-        <Route path="/" element={<Login  />} />
+        <Route path="/" element={<Login />} />
 
         <Route path="/home" element={<Home />} />
 
         <Route path="/login" element={<Login />} />
 
-        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+        <Route path="/temas" element={<ListaTema />} />
+
+        <Route path="/posts" element={<ListaPostagem />} />
+
 
       </Routes>
       <Footer />
