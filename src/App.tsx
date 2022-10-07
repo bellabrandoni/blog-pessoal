1d4cd90d0ebd62880af01ae836b80b37b3be60cf
unknown <isabella14brandao@gmail.com>
2022-10-07 17:36:46 -0300

    Adicionado rotas de temas, posts e formularioPostagem

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import './App.css';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 import ListaTema from './components/temas/listatema/ListaTema';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 
 
@@ -30,9 +34,20 @@ function App() {
 
         <Route path="/posts" element={<ListaPostagem />} />
 
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+<Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+<Route path="/formularioTema" element={<CadastroTema />} />
+
+<Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+<Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+<Route path="/deletarTema/:id" element={<DeletarTema />} />
 
       </Routes>
-      <Footer />
+      <Footer />git
     </BrowserRouter>
 
   );
