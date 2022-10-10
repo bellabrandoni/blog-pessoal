96b0709ebc05a08de02aca5f58f790bd182ba1c1
unknown <isabella14brandao@gmail.com>
2022-10-10 14:31:35 -0300

    Criado componente Modal

--- src/components/postagens/modalPostagem/ModalPostagem.tsx
+++ src/components/postagens/modalPostagem/ModalPostagem.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import { Box } from "@mui/material";
+import { Box, Modal } from "@mui/material";
 import { Button } from "@material-ui/core"
 import CloseIcon from '@material-ui/icons/Close';
 import './ModalPostagem.css';
