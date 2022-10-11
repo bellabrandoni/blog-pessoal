fcdb41bd65c93891afa6c23a9e9a96ca158f5e9a
unknown <isabella14brandao@gmail.com>
2022-10-11 15:14:21 -0300

    Adiconado função selector e tokenSelector

--- src/components/static/navbar/Navbar.tsx
+++ src/components/static/navbar/Navbar.tsx
@@ -2,70 +2,87 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { Box } from "@mui/material";
-import {useNavigate } from 'react-router-dom'
-import useLocalStorage from 'react-use-localstorage';
+import { useNavigate } from 'react-router-dom'
 import './Navbar.css'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducer';
+import { addToken } from '../../../store/tokens/action';
 
 //O botão de Logout precisa excluir o token do localStorage, porque o token é quem garante a autenticalção e é por meio dele    que fazemos nossas requisições na api
 function Navbar() {
-    const [token, setToken] = useLocalStorage('token');
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
     let navigate = useNavigate();
 
+    // dispatch inicializa a ação - disparara a ação armazenada em store. Responsável por enviar o token, a partir dele o usuario consegue logar em qualquer tipo de requisição na nossa api
+    const dispatch = useDispatch();
+
+    // dispatch(addToken('')); -> Modifica o valor para vazio, assim, garantimos que o token será eliminado e por não ter o token ocorre o logout e somos enfim redirecionados para tela de login
     function goLogout() {
-        setToken('')
+        dispatch(addToken(''));
         alert("Usuário deslogado")
         navigate('/login')
     }
-    return (
-        <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box className='cursor'>
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
+
+    var navbarComponent;
+
+    //Se o token for diferente de vazio, significa que há um token e então ele terá que redenrizar o navbar, porém, se for vazio o navbar não será renderizado
+    if (token !== "") {
+        navbarComponent = <AppBar position="static">
+            <Toolbar variant="dense">
+                <Box className='cursor'>
+                    <Typography variant="h5" color="inherit">
+                        BlogPessoal
+                    </Typography>
+                </Box>
+
+                <Box display="flex" justifyContent="start">
+                    <Link to="/home" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="/posts" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="/temas" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                    </Link>
+                    <Link to="/formularioTema" className="text-decorator-none">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                    </Link>
+
+                    <Box mx={1} className='cursor' onClick={goLogout}>
+                        <Typography variant="h6" color="inherit">
+                            logout
                         </Typography>
                     </Box>
 
-                    <Box display="flex" justifyContent="start">
-                        <Link to="/home" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    home
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/posts" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    postagens
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/temas" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    temas
-                                </Typography>
-                            </Box>
-                        </Link>
-                        <Link to="/formularioTema" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
-                                    cadastrar tema
-                                </Typography>
-                            </Box>
-                        </Link>
+                </Box>
 
-                        <Box mx={1} className='cursor' onClick={goLogout}>
-                            <Typography variant="h6" color="inherit">
-                                logout
-                            </Typography>
-                        </Box>
-
-                    </Box>
-
-                </Toolbar>
-            </AppBar>
+            </Toolbar>
+        </AppBar>
+    }
+    return (
+        <>
+            {navbarComponent}
         </>
     )
 }
